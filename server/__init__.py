1a8c3dadfe3bb25fd841a3eb3826bf65d140a798
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-12-20 22:24:23 +0300

    Enable CSRF protection

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from server.views import app_views
 from server.auth import auth
+from flask_wtf.csrf import CSRFProtect
+
+
 
 def create_app():
     """application factory"""
@@ -9,6 +12,9 @@ def create_app():
 
     # load configuration settings
     app.config.from_object('config.ConfigClass')
+
+    # Enable CSRF protection globally
+    csrf = CSRFProtect(app)
 
     app.register_blueprint(app_views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
