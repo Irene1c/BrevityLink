0df1954aeb819ddd6f2b535308065f6e2a3d522c
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-12-21 19:50:31 +0300

    declaring login form fields using flask_wtf

--- server/forms.py
+++ server/forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
-from flask import flash
 
 class SignupForm(FlaskForm):
     username = StringField('Username', validators=[validators.Length(min=2)])
@@ -8,3 +7,8 @@ class SignupForm(FlaskForm):
     password = PasswordField('Password', validators=[validators.Length(min=6)])
     password1 = PasswordField('Confirm Password', validators=[validators.EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[validators.Email()])
+    password = PasswordField('Password', validators=[validators.Length(min=6)])
+    submit = SubmitField('Login')
