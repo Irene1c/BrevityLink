6f87929f511cf43779a791c8e9a5fe57e2612491
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-12-21 21:02:46 +0300

    Implement message flashing and WTForms for home view

--- server/views.py
+++ server/views.py
@@ -1,8 +1,16 @@
-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
+from server.forms import ShortenURLForm
 
 app_views = Blueprint('app_views', __name__)
 
-@app_views.route('/')
+@app_views.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    url_form = ShortenURLForm()
+
+    if url_form.validate_on_submit():
+        long_url = url_form.long_url.data
+    else:
+        for field, errors in url_form.errors.items():
+            flash(f"{url_form[field].label.text}: {errors[0]}", 'error')
+            break
+    return render_template('home.html', form=url_form)
