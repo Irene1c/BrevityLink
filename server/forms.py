8c3ca9b094d8f8e882bd5d6c9c7a62a3adbf5bb0
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-12-27 14:51:17 +0300

    Add comments for the form fields and fix pycodestyle

--- server/forms.py
+++ server/forms.py
@@ -1,18 +1,29 @@
+"""app forms"""
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
 
+
 class SignupForm(FlaskForm):
+    """Defining the signup form"""
+
     username = StringField('Username', validators=[validators.Length(min=2)])
     email = StringField('Email', validators=[validators.Email()])
     password = PasswordField('Password', validators=[validators.Length(min=6)])
-    password1 = PasswordField('Confirm Password', validators=[validators.EqualTo('password')])
+    password1 = PasswordField(
+            'Confirm Password', validators=[validators.EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+
 class LoginForm(FlaskForm):
+    """Defining the login form"""
+
     email = StringField('Email', validators=[validators.Email()])
     password = PasswordField('Password', validators=[validators.Length(min=6)])
     submit = SubmitField('Login')
 
+
 class ShortenURLForm(FlaskForm):
+    """Defining the url form"""
+
     long_url = StringField('URL', validators=[validators.URL()])
     submit = SubmitField('Shorten URL')
