b42ac8080ab63e3b02e4986688545fc531f4e647
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-12-27 14:54:46 +0300

    Defining Database models

---
+++ server/models.py
@@ -0,0 +1,43 @@
+"""Database models"""
+from . import db
+from datetime import datetime
+import random
+import string
+
+
+class User(db.Model):
+    """ class to define each user"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    urls = db.relationship('Url')
+
+
+class Url(db.Model):
+    """ class to define users urls """
+
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String(500))
+    short_url = db.Column(db.String(20), unique=True)
+    clicks = db.Column(db.Integer, default=0)
+    created_date = db.Column(db.DateTime, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.short_url = self.generate_short_url()
+
+    def generate_short_url(self):
+        """Method to generate short url"""
+
+        chars = string.ascii_letters + string.digits
+        short_url = ''.join(random.choices(chars, k=6))
+
+        url = self.query.filter_by(short_url=short_url).first()
+
+        if url:
+            return self.generate_short_url()
+
+        return short_url
