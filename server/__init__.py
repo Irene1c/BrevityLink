a8bbddc15e8c656aae84610318e4480526853d23
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2024-01-03 16:39:52 +0300

    update comments and setup flask_login

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,12 @@
+"""flask app"""
 from flask import Flask
-from server.views import app_views
-from server.auth import auth
-from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 
 db = SQLAlchemy()
-
+login_manager = LoginManager()
 
 def create_app():
     """application factory"""
@@ -19,7 +19,11 @@ def create_app():
     # Enable CSRF protection globally
     csrf = CSRFProtect(app)
 
+    # setup the database
     db.init_app(app)
+
+    from server.views import app_views
+    from server.auth import auth
 
     app.register_blueprint(app_views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
@@ -27,8 +31,20 @@ def create_app():
     # loading models to create the database
     from server.models import User, Url
 
+    # create the database
     with app.app_context():
         db.create_all()
         print('Database created')
+
+    # view when user is not logged in
+    login_manager.login_view = 'auth.login'
+
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        """User loading function"""
+
+        return User.query.get(id)
 
     return app
